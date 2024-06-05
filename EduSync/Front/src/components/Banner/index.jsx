a306369f4a09dd3351662664ab1e988fa0be036f
LeviUNIFOR <147005972+LeviUNIFOR@users.noreply.github.com>
2024-06-05 18:08:20 -0300

    Delete EduSync/Front directory

--- EduSync/Front/src/components/Banner/index.jsx
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import bannerImg from  '../../assets/banner.png'
-
-export function Banner() {
-    return (
-            <img className="banner-image" src= {bannerImg} alt=""/>
-    )
-}
--- EduSync/Front/src/components/Card/index.jsx
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-export function Card(props) {
-    return (
-        <div style={{ width: props.width }} className={'card ' + (props.className ?? '')}>
-            { props.children }
-        </div>
-    )
-}
